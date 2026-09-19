13888467776ab9d61df14fb3e8c914e51346e010
Swalab Tech <238155210+swalabtech@users.noreply.github.com>
2026-09-19 12:29:30 -0700

    chore: bump version to beta.26 (#669)

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,6 @@
 Journiv App Backend - A private journal application.
 """
 
-__version__ = "0.1.0-beta.25"
+__version__ = "0.1.0-beta.26"
 __author__ = "Swalab Tech"
 __description__ = "Private Journal"
